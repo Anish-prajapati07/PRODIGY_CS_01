3a775cd66acc163a253a74c04462fd49f98d1bdb
Anish-prajapati07 <anish.prajapati.ap123@gmail.com>
2024-07-24 19:57:06 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,7 +1,7 @@
 def caesar_encrypt(plaintext, key):
     ciphertext = ""
     for char in plaintext:
-        if char.isalpha():  # Check if the character is a letter
+        if char.isalpha():
             shifted = ord(char) + key
             if char.islower():
                 if shifted > ord('z'):
@@ -20,14 +20,18 @@ def caesar_encrypt(plaintext, key):
 def caesar_decrypt(ciphertext, key):
     plaintext = ""
     for char in ciphertext:
-        if char.isalpha()
+        if char.isalpha():
             shifted = ord(char) - key
             if char.islower():
                 if shifted > ord('z'):
-                    shifted = shifted - 2
+                    shifted = shifted - 26
+                elif shifted < ord('a'):
+                    shifted = shifted + 26
             elif char.isupper():
                 if shifted > ord('Z'):
-                    shifted = shifted - 2
+                    shifted = shifted - 26
+                elif shifted < ord('A'):
+                    shifted = shifted + 26
             plaintext += chr(shifted)
         else:
             plaintext += char
